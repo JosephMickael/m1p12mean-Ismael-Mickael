2466c5ed148ddd888b06eaa3ff3b55be62181e9b
Joseph Mickael <josephmickael03@gmail.com>
2025-04-01 23:11:12 +0300

    suite cors

--- backend/server.js
+++ backend/server.js
@@ -5,20 +5,21 @@ const routes = require('./routes/route')
 
 require('dotenv').config();
 
-const corsOptions = {
-    origin: 'http://localhost:4200',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-
 const app = express()
 const PORT = process.env.PORT || 5001
 console.log(process.env.MONGO_URI);
 
 // Middleware 
-app.use(cors(corsOptions));
+// app.use(cors());
 app.use(express.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    next();
+});
 
 // Connexion à mongoose
 mongoose.connect(process.env.MONGO_URI, {

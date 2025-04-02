df6d2895d1eda9502d4962de43dd131aa71f116a
Ismaël <89607679+kamikaze-droid@users.noreply.github.com>
2025-04-02 21:35:01 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const routes = require('./routes/route')
 require('dotenv').config();
 
 const app = express()
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT 
 console.log(process.env.MONGO_URI);
 
 // Middleware 
@@ -14,7 +14,7 @@ console.log(process.env.MONGO_URI);
 app.use(express.json())
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://garage-m1p12-mean.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://garage-m1p12-mean.vercel.ap');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 

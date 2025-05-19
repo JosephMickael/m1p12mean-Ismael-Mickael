59571dec7100deee672cd01e3d36e7df7e8c6827
kamikaze-droid <narivelotsilavina@gmail.com>
2025-05-19 12:09:11 +0300

    modification taux payement, rajout loader devis

--- backend/controller/Paiement_controller.js
+++ backend/controller/Paiement_controller.js
@@ -1,6 +1,22 @@
 const Stripe = require('stripe'); 
 const Devis = require('../models/Devis'); 
+const axios = require('axios');
+const { error } = require('console');
+
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+const api_key = process.env.API_AXIOS_KEY; 
+const BASE_URL = `https://v6.exchangerate-api.com/v6/${api_key}/latest/USD`; 
+
+
+const obtenirTaux = async (req, res) => {
+    axios.get(BASE_URL).then( response => {
+      const taux = response.data.conversion_rates.MGA/2;
+      console.log(`0.5 $ = ${taux} MGA`); 
+      res.status(200).send( {taux} ); 
+    }).catch(error => {
+      console.error('Erreur lors de la recuperation du taux', error.message); 
+    })
+}
 
 
 const creersession = async (req, res) => {
@@ -25,4 +41,4 @@ const creersession = async (req, res) => {
     res.status(500).send({ error: err.message });
   }
 };
-module.exports = { creersession }; 
+module.exports = { creersession, obtenirTaux }; 

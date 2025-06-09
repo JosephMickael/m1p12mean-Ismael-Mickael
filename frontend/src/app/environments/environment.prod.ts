5375721a0b10e7fda5fad7aecb8d95f929ac60b8
Joseph Mickael <josephmickael03@gmail.com>
2025-06-09 20:20:01 +0300

    Render Kamikaze

--- frontend/src/app/environments/environment.prod.ts
+++ frontend/src/app/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://m1p12mean-ismael-mickael-6fid.onrender.com/garage_api'
-    // apiUrl: 'https://m1p12mean-ismael-mickael.onrender.com/garage_api'
+    // apiUrl: 'https://m1p12mean-ismael-mickael-6fid.onrender.com/garage_api'
+    apiUrl: 'https://m1p12mean-ismael-mickael.onrender.com/garage_api'
 }
 // export const environment = {
 //     production: false,

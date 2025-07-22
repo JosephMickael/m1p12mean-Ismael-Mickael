571ac6818cbc8b5b443bd9d7ce9881eda4c1ec0d
Joseph Mickael <josephmickael03@gmail.com>
2025-07-22 22:23:48 +0300

    Correction bugs

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(express.json())
 const corsOptions = {
     origin: (origin, callback) => {
         const allowedOrigins = [
-            'https://m1p12mean-ismael-mickael.vercel.app',
+            'https://m1p12mean-mickael-ismael.vercel.app',
             'http://localhost:4200'
         ];
 

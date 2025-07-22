e9deb5150145fcfac8a189343755e950ff516130
Joseph Mickael <josephmickael03@gmail.com>
2025-07-22 12:07:06 +0300

    prod false

--- frontend/src/app/environments/environment.prod.ts
+++ frontend/src/app/environments/environment.prod.ts
@@ -1,9 +1,9 @@
-export const environment = {
-    production: true,
-    // apiUrl: 'https://m1p12mean-ismael-mickael-6fid.onrender.com/garage_api'
-    apiUrl: 'https://m1p12mean-ismael-mickael.onrender.com/garage_api'
-}
 // export const environment = {
-//     production: false,
-//     apiUrl: 'http://localhost:5001/garage_api'
+//     production: true,
+//     // apiUrl: 'https://m1p12mean-ismael-mickael-6fid.onrender.com/garage_api'
+//     apiUrl: 'https://m1p12mean-ismael-mickael.onrender.com/garage_api'
 // }
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:5001/garage_api'
+}
